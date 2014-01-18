f19a1573ba74bcb3fe0672f2ead37134e2a0d44f
Jan Meier <jan@2freunde.org>
2014-01-18 18:02:38 +0100

    solved b12 comp

--- src/ueb/b/aufg12_comp.py
+++ src/ueb/b/aufg12_comp.py
@@ -5,8 +5,13 @@
 from vl.lek03.dfs import dfs
 
 def comp(G):
-    C = []  
-    # todo
+    V = {i for i in range(len(G)) }
+    C = []
+    while V:
+        v = V.pop()
+        E,_ = dfs(G, v) 
+        C.append(E)
+        V  -= E
     return C
 
 
