fac0807daed2b71528094e54b3d1608e30ce42f6
Jan Meier <jan@2freunde.org>
2014-01-18 15:50:12 +0100

    solved b10 dfs_rec

--- src/ueb/b/aufg10_dfs_rec.py
+++ src/ueb/b/aufg10_dfs_rec.py
@@ -3,7 +3,17 @@
 # drittes Argument kann weggelassen werden, weil default-Wert vorhanden
 # default-Wert wird bereits bei Fktdeklaration gesetzt
 def dfs_rec(G, u, explored = None):
-    # todo
+    if(not explored):
+        explored = [False for _ in G]
+    # u is visited because we are looking at it now
+    explored[u] = True
+    # for all adjacent vertices... 
+    for v in G[u]:
+        # that are not already visited ... 
+        if not explored[v]:
+            # look @ the subtree
+            explored = dfs_rec(G,v,explored)
+    # return explored list
     return explored
 
 
