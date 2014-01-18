da6e50672b677d7c578cc515779a19663a974f44
Jan Meier <jan@2freunde.org>
2014-01-18 16:43:52 +0100

    solved b11 bfs_numpaths

--- src/ueb/b/aufg11_bfs_numpaths.py
+++ src/ueb/b/aufg11_bfs_numpaths.py
@@ -4,8 +4,16 @@
 from vl.lek03.bfs import bfs
 
 def bfs_numpaths(G,s):
-    # todo
-    return []
+    L,P = bfs(G, s)
+    S = [0 for _ in G]
+    S[s] = 1
+    for i in range(1,len(L)):  
+        for u in L[i]: 
+            for v in L[i-1]:
+                if(u in G[v]): 
+                    S[u] += S[v]
+                
+    return S
 
 
 def define_G():
