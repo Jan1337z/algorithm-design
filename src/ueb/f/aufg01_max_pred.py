c5663eae5434e12e7ca0fd621f96f464b363ae38
Jan Meier <jan@2freunde.org>
2014-01-21 16:45:38 +0100

    changed f1 max_predecessors (removed output in file)

--- src/ueb/f/aufg01_max_pred.py
+++ src/ueb/f/aufg01_max_pred.py
@@ -40,7 +40,7 @@ def max_predecessors(L):
 
 
 # Bsp 10.2 aus der VL
-L = [(0,3,2),(1,5,4),(4,6,4),(2,9,7),(7,10,2),(8,11,1)]
-print(max_predecessors(L))
-
-print(max_predecessors(L) == [-1, -1, 0, -1, 2, 2])
+# L = [(0,3,2),(1,5,4),(4,6,4),(2,9,7),(7,10,2),(8,11,1)]
+# print(max_predecessors(L))
+# 
+# print(max_predecessors(L) == [-1, -1, 0, -1, 2, 2])
